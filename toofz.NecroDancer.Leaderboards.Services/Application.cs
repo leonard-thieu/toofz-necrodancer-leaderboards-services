450b80fbdfe89fd20231316b9d266a8ec25d1c62
Leonard Thieu <leonard.thieu@gmail.com>
2017-08-19 14:15:27 -0400

    Remove App Insights-related code.

--- toofz.NecroDancer.Leaderboards.Services/Application.cs
+++ toofz.NecroDancer.Leaderboards.Services/Application.cs
@@ -4,28 +4,12 @@ using System.Reflection;
 using System.ServiceProcess;
 using System.Threading.Tasks;
 using log4net;
-using Microsoft.ApplicationInsights;
 
 namespace toofz.NecroDancer.Leaderboards.Services
 {
     public static class Application
     {
         static readonly ILog Log = LogManager.GetLogger(typeof(Application));
-
-        public static readonly TelemetryClient TelemetryClient = InitializeTelemetry();
-
-        // TODO: Make this a TelemetryInitializer
-        static TelemetryClient InitializeTelemetry()
-        {
-            var telemetryClient = new TelemetryClient();
-
-            var context = telemetryClient.Context;
-            context.User.Id = Environment.UserName;
-            context.Session.Id = Guid.NewGuid().ToString();
-            context.Device.OperatingSystem = Environment.OSVersion.ToString();
-
-            return telemetryClient;
-        }
 
         public static void Run<T, TSettings>()
             where T : WorkerRoleBase, new()
