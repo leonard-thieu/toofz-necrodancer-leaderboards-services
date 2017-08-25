1889fcfece4a16dbfb291976924ac823c55099a2
Leonard Thieu <leonard.thieu@gmail.com>
2017-08-25 09:57:26 -0400

    Rename toofz.NecroDancer.Leaderboards.Services to toofz.Services.

--- toofz.Services.Tests/Properties/Settings.Designer.cs
+++ toofz.Services.Tests/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace toofz.NecroDancer.Leaderboards.Services.Tests.Properties {
+namespace toofz.Services.Tests.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
