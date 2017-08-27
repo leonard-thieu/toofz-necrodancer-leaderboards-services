b0886f17a18f0d7598ba6728af8565f8b1d56b80
Leonard Thieu <leonard.thieu@gmail.com>
2017-08-27 10:52:42 -0400

    v6.0.0

--- toofz.Services/Properties/AssemblyInfo.cs
+++ toofz.Services/Properties/AssemblyInfo.cs
@@ -1,7 +1,7 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("5.3.0.0")]
+[assembly: AssemblyVersion("6.0.0.0")]
 
 [assembly: AssemblyCopyright("Copyright © Leonard Thieu 2017")]
 [assembly: AssemblyProduct("toofz")]
