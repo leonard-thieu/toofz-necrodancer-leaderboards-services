118214e2f5bd4e012c63382f2eb83bf7501656ce
Leonard Thieu <leonard.thieu@gmail.com>
2017-08-27 10:49:37 -0400

    Make Application instantiable and implement an interface.

---
+++ toofz.Services/IApplication.cs
@@ -0,0 +1,9 @@
+﻿namespace toofz.Services
+{
+    public interface IApplication
+    {
+        void Run<T, TSettings>()
+            where T : WorkerRoleBase<TSettings>, new()
+            where TSettings : ISettings;
+    }
+}
