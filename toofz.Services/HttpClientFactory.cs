ca57c5f534af99370c046f516f31911f56ff94c5
Leonard Thieu <leonard.thieu@gmail.com>
2017-11-04 20:34:26 -0400

    Add helper method to create an HTTP pipeline.

---
+++ toofz.Services/HttpClientFactory.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+
+namespace toofz.Services
+{
+    public static class HttpClientFactory
+    {
+        public static HttpMessageHandler CreatePipeline(HttpMessageHandler innerHandler, IEnumerable<DelegatingHandler> handlers)
+        {
+            if (innerHandler == null)
+                throw new ArgumentNullException(nameof(innerHandler));
+            if (handlers == null)
+                return innerHandler;
+
+            var httpMessageHandler = innerHandler;
+
+            foreach (var delegatingHandler in handlers.Reverse())
+            {
+                if (delegatingHandler == null)
+                    throw new ArgumentNullException(nameof(handlers));
+                if (delegatingHandler.InnerHandler != null)
+                    throw new ArgumentException("DelegatingHandler has non-null inner handler.", nameof(handlers));
+
+                delegatingHandler.InnerHandler = httpMessageHandler;
+                httpMessageHandler = delegatingHandler;
+            }
+
+            return httpMessageHandler;
+        }
+    }
+}
